24b58a3b7d64f3c8b771340471c07e82a0ed5e54
edan <emanueledancelli@gmail.com>
2019-03-04 14:55:15 +0100

    refactor: media queries

--- src/styles/mediaQueries.js
+++ src/styles/mediaQueries.js
@@ -1,3 +1,3 @@
 const breakpoints = [576, 768, 940, 1200];
 
-export const mq = breakpoints.map(bp => `@media (min-width: ${bp}px)`);
+export const mq = breakpoints.map(e => `@media (min-width: ${e}px)`);
